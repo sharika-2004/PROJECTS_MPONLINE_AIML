00f422cc5b50ccb68817bb89a5011d8d9b87900d
Sharika P K <sharikavipanchika@gmail.com>
2026-06-25 20:36:25 +0530

    Create model.py

---
+++ Cancer Detection/Implementation/model.py
@@ -0,0 +1,28 @@
+base_model = MobileNetV2(input_shape=(128, 128, 3), include_top=False, weights='imagenet')
+base_model.trainable = False
+
+model = models.Sequential([
+    base_model,
+    layers.GlobalAveragePooling2D(),
+    layers.Dense(64, activation='relu'),
+    layers.Dropout(0.5),
+    layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+model.summary()
+
+EPOCHS = 5
+BATCH_SIZE = 32
+
+history = model.fit(
+    X_train, y_train,
+    validation_data=(X_val, y_val),
+    epochs=EPOCHS,
+    batch_size=BATCH_SIZE
+)
