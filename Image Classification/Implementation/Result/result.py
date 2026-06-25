795b2de48a97c0c446c9344d099893bfa1c8c137
Sharika P K <sharikavipanchika@gmail.com>
2026-06-25 19:41:43 +0530

    Create result.py

---
+++ Image Classification/Implementation/Result/result.py
@@ -0,0 +1,20 @@
+Test Loss : 0.19891558587551117
+Test Accuracy : 0.930899977684021
+
+              precision    recall  f1-score   support
+
+ T-shirt/Top       0.91      0.86      0.88      1000
+     Trouser       0.99      0.99      0.99      1000
+    Pullover       0.90      0.90      0.90      1000
+       Dress       0.93      0.95      0.94      1000
+        Coat       0.89      0.91      0.90      1000
+      Sandal       0.99      0.98      0.99      1000
+       Shirt       0.79      0.80      0.79      1000
+     Sneaker       0.95      0.98      0.97      1000
+         Bag       0.99      0.99      0.99      1000
+  Ankle Boot       0.98      0.96      0.97      1000
+
+    accuracy                           0.93     10000
+   macro avg       0.93      0.93      0.93     10000
+weighted avg       0.93      0.93      0.93     10000
+
