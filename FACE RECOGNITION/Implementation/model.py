5901dd875de55f75cc22ec704d748a2f740df3d1
Sharika P K <sharikavipanchika@gmail.com>
2026-06-25 21:01:59 +0530

    Create model.py

---
+++ FACE RECOGNITION/Implementation/model.py
@@ -0,0 +1,18 @@
+base_model = MobileNetV2(input_shape=(128, 128, 3), include_top=False, weights='imagenet')
+base_model.trainable = False
+
+model = models.Sequential([
+    base_model,
+    layers.GlobalAveragePooling2D(),
+    layers.Dense(64, activation='relu'),
+    layers.Dropout(0.4),
+    layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+model.summary()
